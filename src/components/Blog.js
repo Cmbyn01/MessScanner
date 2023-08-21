0eb6ac1477f39815b48748e28f549a884b0d7105
muskan <muskanpurohit01@gmail.com>
2023-08-21 16:56:33 +0530

    Changes in height

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -10,7 +10,7 @@ const Blog = () => {
     
     <section className="text-gray-600 body-font">
       <Hero />
-      <div className="container mx-auto flex flex-col px-5 py-24 items-center justify-center">
+      <div className="container mx-auto flex flex-col px-5 py-12 items-center justify-center">
         <div className="text-center lg:w-2/3 w-full">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Clink Here to Scan Student ID</h1>
           <div className="flex justify-center mt-4">
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import logoImage from './images/WhatsApp Image 2023-08-15 at 18.39.41.jpeg'
 
 const Header = () => {
   return (
-    <header className="text-gray-600 body-font border-b border-gray-200">
+    <header className="text-gray-600 body-font border-b bg-gray-100 border-gray-200">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-    <img src={logoImage} alt="Logo" className="w-10 h-10 text-white p-2 rounded-full" />
+    <img src={logoImage} alt="Logo" className="w-10 h-10 text-white p-2" />
       {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg> */}
